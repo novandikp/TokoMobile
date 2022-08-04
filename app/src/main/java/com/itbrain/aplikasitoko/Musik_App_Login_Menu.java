de972f82607fe80cceb9b8424b62a428277c7417
mpuss37 <herdiseptanto2017@gmail.com>
2022-08-04 09:52:49 +0700

    upload aplikasi ( 1.Rental Mobil 2.Tabungan 3.Laundry 4.Pos Toko Kredit 5.Antrian Portable 6.Aplikasi Cetak Kwitansi 7.Toko Sepatu )

---
+++ app/src/main/java/com/itbrain/aplikasitoko/Musik_App_Login_Menu.java
@@ -0,0 +1,14 @@
+package com.itbrain.aplikasitoko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Musik_App_Login_Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.musik_app_login_menu);
+    }
+}
---
+++ app/src/main/java/com/itbrain/aplikasitoko/Onboarding_Super_App3.java
@@ -0,0 +1,14 @@
+package com.itbrain.aplikasitoko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Onboarding_Super_App3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.onboarding_super_app3);
+    }
+}
