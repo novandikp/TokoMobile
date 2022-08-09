d0c61fb29891dd0a211c8ebe056a0dabd9317fb2
mpuss37 <herdiseptanto2017@gmail.com>
2022-08-09 14:11:50 +0700

    laundry

--- app/src/main/java/com/itbrain/aplikasitoko/MenuLaporanPendapatanLaundry.java
+++ app/src/main/java/com/itbrain/aplikasitoko/MenuLaporanPendapatanLaundry.java
@@ -2,7 +2,9 @@ package com.itbrain.aplikasitoko;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuLaporanPendapatanLaundry extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MenuLaporanPendapatanLaundry extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_laporan_pendapatan_laundry);
+    }
+
+    public void Excel(View view) {
+        Intent intent = new Intent(MenuLaporanPendapatanLaundry.this, ItemLaporanPendapatanLaundry.class);
+        startActivity(intent);
     }
 }
