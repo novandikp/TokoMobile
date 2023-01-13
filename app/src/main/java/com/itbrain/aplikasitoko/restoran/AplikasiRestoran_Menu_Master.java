3452d09a90b5f2816cc9d7b492fee330a5a6d16d
HafizzzPro <122583812+HafizzzPro@users.noreply.github.com>
2023-01-13 12:58:30 +0700

    cek

--- app/src/main/java/com/itbrain/aplikasitoko/restoran/AplikasiRestoran_Menu_Master.java
+++ app/src/main/java/com/itbrain/aplikasitoko/restoran/AplikasiRestoran_Menu_Master.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.ImageButton;
 
 import com.itbrain.aplikasitoko.R;
-import com.itbrain.aplikasitoko.TambahMeja;
 
 public class AplikasiRestoran_Menu_Master extends AppCompatActivity {
 
