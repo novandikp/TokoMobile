9f4b872d2f184af82056114973a5c60c3a4a7935
iwaklele45 <rafirutra027@gmail.com>
2023-01-13 11:00:17 +0700

    upload aplikasi ( 1.Rental Mobil 2.Tabungan 3.Laundry 4.Pos Toko Kredit 5.Antrian Portable 6.Aplikasi Cetak Kwitansi 7.Toko Sepatu )

---
+++ app/src/main/java/com/itbrain/aplikasitoko/MenuBayarPengembalianKendaraanMobil.java
@@ -0,0 +1,14 @@
+package com.itbrain.aplikasitoko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuBayarPengembalianKendaraanMobil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_bayar_pengembalian_kendaraan_mobil);
+    }
+}
---
+++ app/src/main/java/com/itbrain/aplikasitoko/Menu_Riwayat_Transaksi_Super_App.java
@@ -0,0 +1,14 @@
+package com.itbrain.aplikasitoko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu_Riwayat_Transaksi_Super_App extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_riwayat_transaksi_super_app);
+    }
+}
