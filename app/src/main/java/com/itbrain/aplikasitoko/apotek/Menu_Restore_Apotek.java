524af561661b3f7808f6460631c08493024236ad
Fawawaz <zildan2804@gmail.com>
2023-02-22 11:38:59 +0700

    aplut apotek masbro

--- app/src/main/java/com/itbrain/aplikasitoko/apotek/Menu_Restore_Apotek.java
+++ app/src/main/java/com/itbrain/aplikasitoko/apotek/Menu_Restore_Apotek.java
@@ -181,8 +181,8 @@ class AdapterBackup extends ArrayAdapter<String> {
         String[] row_items = records.get(position).split("__");
 
         TextView nama= (TextView) itemView.findViewById(R.id.text);
-        Button b = (Button) itemView.findViewById(R.id.hapus) ;
-        ConstraintLayout wadah = (ConstraintLayout) itemView.findViewById(R.id.wadah) ;
+        Button b = (Button) itemView.findViewById(R.id.hapusRestore) ;
+        ConstraintLayout wadah = (ConstraintLayout) itemView.findViewById(R.id.wadahRestore) ;
 
         nama.setText(row_items[0]);
         b.setTag(row_items[1]);
